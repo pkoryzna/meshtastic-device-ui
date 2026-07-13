c3b13ff11793157b74a96dad19bad42be786f140
Patryk Koryzna <patryk@koryzna.com>
2026-07-13 22:27:07 +0200

    fix display callback type

--- include/graphics/driver/ESPLCDDriver.h
+++ include/graphics/driver/ESPLCDDriver.h
@@ -22,7 +22,7 @@ public:
     static ESPLCDDriver &create(uint16_t width, uint16_t height);
 protected:
     static void lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map);
-    static void notify_lvgl_flush_ready(esp_lcd_panel_handle_t panel, esp_lcd_dpi_panel_event_data_t *edata, void *user_ctx);
+    static bool notify_lvgl_flush_ready(esp_lcd_panel_handle_t panel, esp_lcd_dpi_panel_event_data_t *edata, void *user_ctx);
     static void enable_dsi_phy_power(void);
     
 private:
